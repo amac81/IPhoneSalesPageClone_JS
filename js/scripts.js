ccf7fb14fe8d68d63e81731a9f1bccf7ce4882ad
amac81 <arnaldocanelas@gmail.com>
2023-10-17 17:19:36 +0100

    CSS update

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//element selection
+
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector("#product-image");
+
+const btnSilver = document.querySelector("#silver");
+
+
+   console.log(btnSilver);
